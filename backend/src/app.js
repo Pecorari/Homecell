58a4e5f887cf5f7b7f904d2fe061c60f0621e2d4
Pecorari <pecorari.clemente@gmail.com>
2025-03-08 16:08:31 -0300

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ BigInt.prototype.toJSON = function () {
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://homecellofficial.com.br/static/js/',
+  origin: 'https://homecellofficial.com.br',
   credentials: true,
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type',
