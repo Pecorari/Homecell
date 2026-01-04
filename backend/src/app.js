796ebea6685078b72aa61dc98272f86ec5699555
Pecorari <pecorari.clemente@gmail.com>
2026-01-04 16:08:14 -0300

    Alterando origin do CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ BigInt.prototype.toJSON = function () {
 app.use(express.json());
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN_TEST,
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
 };
