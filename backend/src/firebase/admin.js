8aabd4576e32aa01ea4124bdf82ac73d60339625
Pecorari <pecorari.clemente@gmail.com>
2026-01-04 17:19:26 -0300

    Especificando qual o bucket do firebase

--- backend/src/firebase/admin.js
+++ backend/src/firebase/admin.js
@@ -1,7 +1,8 @@
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.applicationDefault(),
+  storageBucket: "homecell-firebase.firebasestorage.app"
 });
 
 module.exports = admin;
